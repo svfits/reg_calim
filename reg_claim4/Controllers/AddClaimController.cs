66e44e494fa889b685930e222d6bba81d15caff7
svfits <a1d1@inbox.ru>
2016-02-21 14:45:01 +0800

    логин или по имени выборка

--- reg_claim4/Controllers/AddClaimController.cs
+++ reg_claim4/Controllers/AddClaimController.cs
@@ -9,100 +9,42 @@ namespace reg_claim4.Controllers
 {
     public  class AddClaimController : Controller
     {
-       
+        userdbContext db = new userdbContext();
         /// <summary>
         /// для заведения Заявок вывод запросов из БД во время заведения
         /// </summary>              
-                
-        public  ActionResult WhomUserClaimFromAD(string term)
-        {           
-            using (userdbContext db = new userdbContext())
-            {
-                var user = db.Ad_users;
 
-                var userName = user
-                        .Where(c => c.UserName.StartsWith(term))
+        public  ActionResult WhomUserClaimFromAD(string term)
+        {              
+                var DisplayName1 = db.Ad_users
+                    .Where(c => c.DisplayName.StartsWith(term))
+                    .Select(c => c.DisplayName)
+                    .AsEnumerable()
+                    .Distinct()
+                    .Take(10)
+                    .ToList();
+
+                if (DisplayName1.Count > 0)
+                {
+                System.Diagnostics.Debug.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>       " + DisplayName1.Count);
+                return Json(DisplayName1, JsonRequestBehavior.AllowGet);
+                }
+                
+                var userName1 = db.Ad_users
+                     .Where(c => c.UserName.StartsWith(term))
                         .Select(c => c.UserName)
                         .AsEnumerable()
                         .Distinct()
                         .Take(10)
                         .ToList();
 
-                if (userName.Count >= 0)
+                if (userName1.Count > 0)
                 {
-                    return Json(userName, JsonRequestBehavior.AllowGet);
+                    return Json(userName1, JsonRequestBehavior.AllowGet);
                 }
-
-                //foreach (var item in user)
-                //{
-                //    // System.Diagnostics.Debug.WriteLine(item.Email.ToString());
-                //    // System.Diagnostics.Debug.WriteLine(item.UserName.ToString());
-                //    System.Diagnostics.Debug.WriteLine(item.DisplayName.ToString());
-                //}
-
-                var user2 = db.Ad_users;
-
-                var DisplayName = user2
-                      .Where(c => c.DisplayName.StartsWith(term))
-                      .Select(c => c.DisplayName)
-                      .AsEnumerable()
-                      .Distinct()
-                      .Take(10)
-                      .ToList();
-
-                if (DisplayName.Count >= 0)
-                {
-                    return Json(DisplayName, JsonRequestBehavior.AllowGet);
-                }
-                return Json("пользователь не найден", JsonRequestBehavior.AllowGet);
-            }
-
-            //    try
-            //    {
-            //        //var user = db.Ad_users
-            //    .Where(c => c.UserName.StartsWith(term))
-            //    .Select(c => c.UserName)
-            //    .AsEnumerable()
-            //    .Distinct()
-            //    .Take(10)
-            //    .ToList();
-
-           
-
-            //System.Diagnostics.Debug.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-
-            //if (user.Count <= 0)
-            //{
-            //    //System.Diagnostics.Debug.WriteLine("не найден пользователь!!!!!!!!!!!!!!!!!!!");
-            //    //  user = db.Ad_users
-            // .Where(c => c.DisplayName.StartsWith(term))
-            // .AsEnumerable()
-            // .Take(10)
-            // .ToList();
-
-            //  foreach (var item in user)
-            //  {
-            //     // System.Diagnostics.Debug.WriteLine(item.Email.ToString());
-            //     // System.Diagnostics.Debug.WriteLine(item.UserName.ToString());
-            //      System.Diagnostics.Debug.WriteLine(item.DisplayName.ToString());
-
-
-            //  }
-
-            //        return Json(user, JsonRequestBehavior.AllowGet);
-            //    }
-            //    else
-            //    {
-            //        return Json(user, JsonRequestBehavior.AllowGet);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    System.Diagnostics.Debug.WriteLine(ex.ToString());
-            //    System.Diagnostics.Debug.WriteLine("ошибка пользователь не найден ");
-            //    return Json("что то пошло не так тут", JsonRequestBehavior.AllowGet);
-            //}
-
+                       
+            return Json("пользователь не найден", JsonRequestBehavior.AllowGet);
+            
         }
       
     }
