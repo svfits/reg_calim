c6be1f899afc5edb331dc7718bba44994a1fe175
svfits <a1d1@inbox.ru>
2016-02-29 13:34:45 +0800

    промежудочный

---
+++ reg_claim4/Scripts/autocomplecte_addClaim.js
@@ -0,0 +1,44 @@
+﻿
+      //кому зявка
+  $(function () {
+      $("[data]").each(function () {
+          var target = $(this);
+          target.autocomplete({ source: target.attr("data") });
+      });
+  });
+
+
+//название заявки и вставить дату
+$(function () {
+    $("[dataClaimeName]").each(function () {
+        var target = $(this);
+        target.autocomplete({ source: target.attr("dataClaimeName") });
+        target.autocomplete({
+            select: function (event, ui) {
+                console.log(ui.item.value);                    
+                dataTimeEnd(ui.item.value);
+            }
+        });
+    });
+});
+
+function dataTimeEnd(date_end) {
+    $.ajax({
+        type: "POST",
+        url: "/AddClaim/DataTimeEnd",
+        data: {
+            date_end: date_end
+        },
+        success: function (data) {
+            document.getElementById("dataTimeEnd").innerHTML = data;
+        }
+    });
+}
+
+// откого заявка по умолчанию тот кто открыл страницу
+$(function () {
+    $("[whoAmiFromClaim]").each(function () {
+        var target = $(this);
+        target.autocomplete({ source: target.attr("whoAmiFromClaim") });
+    });
+});
